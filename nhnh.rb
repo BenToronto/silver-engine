dd374d6ff4f7afecad98401c3cae8925f3969939
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-12-05 20:00:07 -0500

    Create nhnh.rb

---
+++ nhnh.rb
@@ -0,0 +1,34 @@
+Class tillas {
+    number geter_io_plan
+    number power
+    Characters do_asp_star
+    number aspoagb
+}
+Function xfghgxh {
+}
+
+Function hhgh {
+}
+
+class Customer {
+    @@yes_no = 99
+    cust1 = Customer. new
+cust2 = Customer. new
+ def initialize(id, name, addr)
+      @cust_id = id
+      @cust_name = name
+      @cust_addr = addr
+   end
+end
+}
+#!/usr/bin/ruby
+
+class Sample
+   def hello
+      puts "Hello Ruby!"
+   end
+end
+
+# Now using above class to create objects
+object = Sample. new
+object.hello
